41ab1f15fc39d6e1d46ce416b8362e8c0388c21d
James Smith <james.smith@djalbat.com>
2025-02-18 20:10:42 +0000

    Progress sifting through the characters.

--- src/example/view.js
+++ src/example/view.js
@@ -11,112 +11,37 @@ import { shapeCharacterMap,
          arrowCharacterMap,
          harpoonCharacterMap,
          equalityCharacterMap,
+         orderingCharacterMap,
+         calculusCharacterMap,
+         relationalCharacterMap,
+         arithmeticCharacterMap,
          greekLetterCharacterMap,
          headedArrowCharacterMap,
+         scriptLettersCharacterMap,
          circleCircledCharacterMap,
+         tackTurnstileCharacterMap,
          classTheoreticCharacterMap,
+         frakturLettersCharacterMap,
          logicalOperatorCharacterMap,
          doubleTripleArrowCharacterMap,
-         isomorphismSymbolCharacterMap,
-         relationalOperatorCharacterMap,
-         mathematicalScriptCharacterMap,
-         arithmeticOperatorCharacterMap,
-         derivativeIntegralCharacterMap,
          parenthesisBracketCharacterMap,
-         mathematicalFrakturCharacterMap,
+         doubleStruckLettersCharacterMap,
          superscriptSubscriptCharacterMap,
-         miscellaneousOperatorCharacterMap,
-         mathematicalDoubleStruckCharacterMap } from "../unicodeCharacterMap";
+         miscellaneousOperatorCharacterMap } from "../unicodeCharacterMap";
 
 export default class View extends Element {
   childElements() {
+    const parameters = parametersFromNothing(),
+          { name, title } = parameters,
+          characterMap = characterMapFromName(name);
+
     return ([
 
       <MainHeader/>,
       <SecondaryHeading>
-        Miscellaneous Operator
+        {title}
       </SecondaryHeading>,
-      <GlyphsTable characterMap={miscellaneousOperatorCharacterMap} />,
-      <SecondaryHeading>
-        Class Theoretic
-      </SecondaryHeading>,
-      <GlyphsTable characterMap={classTheoreticCharacterMap} />,
-      <SecondaryHeading>
-        Angle
-      </SecondaryHeading>,
-      <GlyphsTable characterMap={angleCharacterMap} />,
-      <SecondaryHeading>
-        Logical Operator
-      </SecondaryHeading>,
-      <GlyphsTable characterMap={logicalOperatorCharacterMap} />,
-      <SecondaryHeading>
-        Shape
-      </SecondaryHeading>,
-      <GlyphsTable characterMap={shapeCharacterMap} />,
-      <SecondaryHeading>
-        Parenthesis Bracket
-      </SecondaryHeading>,
-      <GlyphsTable characterMap={parenthesisBracketCharacterMap} />,
-      <SecondaryHeading>
-        Equality
-      </SecondaryHeading>,
-      <GlyphsTable characterMap={equalityCharacterMap} />,
-      <SecondaryHeading>
-        Arithmetic Operator
-      </SecondaryHeading>,
-      <GlyphsTable characterMap={arithmeticOperatorCharacterMap} />,
-      <SecondaryHeading>
-        Derivative Integral
-      </SecondaryHeading>,
-      <GlyphsTable characterMap={derivativeIntegralCharacterMap} />,
-      <SecondaryHeading>
-        Circle Circled
-      </SecondaryHeading>,
-      <GlyphsTable characterMap={circleCircledCharacterMap} />,
-      <SecondaryHeading>
-        Arrow
-      </SecondaryHeading>,
-      <GlyphsTable characterMap={arrowCharacterMap} />,
-      <SecondaryHeading>
-        Relational Operator
-      </SecondaryHeading>,
-      <GlyphsTable characterMap={relationalOperatorCharacterMap} />,
-      <SecondaryHeading>
-        Harpoon
-      </SecondaryHeading>,
-      <GlyphsTable characterMap={harpoonCharacterMap} />,
-      <SecondaryHeading>
-        Double Triple Arrow
-      </SecondaryHeading>,
-      <GlyphsTable characterMap={doubleTripleArrowCharacterMap} />,
-      <SecondaryHeading>
-        Headed Arrow
-      </SecondaryHeading>,
-      <GlyphsTable characterMap={headedArrowCharacterMap} />,
-      <SecondaryHeading>
-        Superscript Subscript
-      </SecondaryHeading>,
-      <GlyphsTable characterMap={superscriptSubscriptCharacterMap} />,
-      <SecondaryHeading>
-        Isomorphism
-      </SecondaryHeading>,
-      <GlyphsTable characterMap={isomorphismSymbolCharacterMap} />,
-      <SecondaryHeading>
-        Greek Letter
-      </SecondaryHeading>,
-      <GlyphsTable characterMap={greekLetterCharacterMap} />,
-      <SecondaryHeading>
-        Mathematical Script
-      </SecondaryHeading>,
-      <GlyphsTable characterMap={mathematicalScriptCharacterMap} />,
-      <SecondaryHeading>
-        Mathematical Fraktur
-      </SecondaryHeading>,
-      <GlyphsTable characterMap={mathematicalFrakturCharacterMap} />,
-      <SecondaryHeading>
-        Mathematical Double Struck
-      </SecondaryHeading>,
-      <GlyphsTable characterMap={mathematicalDoubleStruckCharacterMap} />
+      <GlyphsTable characterMap={characterMap} />
 
     ]);
   }
@@ -126,4 +51,52 @@ export default class View extends Element {
   static defaultProperties = {
     className: "view"
   };
+}
+
+function parametersFromNothing() {
+  const { search } = location,
+        start = 1,
+        queryString = search.substring(start),
+        nameValueStrings = queryString.split("&"),
+        parameters = nameValueStrings.reduce((parameters, nameValueString) => {
+          const [ name, encodedValue ] = nameValueString.split("="),
+                value = encodedValue.replace(/\+/g, " ");
+
+          parameters[name] = value;
+
+          return parameters;
+        }, {});
+
+  return parameters;
+}
+
+function characterMapFromName(name) {
+  let characterMap;
+
+  switch (name) {
+    case "shape": characterMap = shapeCharacterMap; break;
+    case "angle": characterMap = angleCharacterMap; break;
+    case "arrow": characterMap = arrowCharacterMap; break;
+    case "harpoon": characterMap = harpoonCharacterMap; break;
+    case "equality": characterMap = equalityCharacterMap; break;
+    case "ordering": characterMap = orderingCharacterMap; break;
+    case "calculus": characterMap = calculusCharacterMap; break;
+    case "relational": characterMap = relationalCharacterMap; break;
+    case "arithmetic": characterMap = arithmeticCharacterMap; break;
+    case "greekLetter": characterMap = greekLetterCharacterMap; break;
+    case "headedArrow": characterMap = headedArrowCharacterMap; break;
+    case "scriptLetters": characterMap = scriptLettersCharacterMap; break;
+    case "circleCircled": characterMap = circleCircledCharacterMap; break;
+    case "tackTurnstile": characterMap = tackTurnstileCharacterMap; break;
+    case "classTheoretic": characterMap = classTheoreticCharacterMap; break;
+    case "frakturLetters": characterMap = frakturLettersCharacterMap; break;
+    case "logicalOperator": characterMap = logicalOperatorCharacterMap; break;
+    case "doubleTripleArrow": characterMap = doubleTripleArrowCharacterMap; break;
+    case "parenthesisBracket": characterMap = parenthesisBracketCharacterMap; break;
+    case "doubleStruckLetters": characterMap = doubleStruckLettersCharacterMap; break;
+    case "superscriptSubscript": characterMap = superscriptSubscriptCharacterMap; break;
+    case "miscellaneousOperator": characterMap = miscellaneousOperatorCharacterMap; break;
+  }
+
+  return characterMap;
 }
--- src/example/view/table/glyphs.js
+++ src/example/view/table/glyphs.js
@@ -38,3 +38,4 @@ export default withStyle(GlyphsTable)`
   border-collapse: collapse;
   
 `;
+
