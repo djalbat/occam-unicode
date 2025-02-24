cd72d3d6fd5ad4a32b3cbb6e831f509475391515
James Smith <james.smith@djalbat.com>
2025-02-24 18:36:32 +0000

    Propagated.

--- src/sizes.js
+++ src/sizes.js
@@ -1,6 +1,12 @@
 "use strict";
 
-export const halfSize = "0.8rem";
 export const quarterSize = "0.4rem";
+export const halfSize = "0.8rem";
+export const smallSize = "1.2rem";
+export const mediumSize = "1.6rem";
+export const normalSize = "1.7rem";
+export const fullSize = "2.0rem";
 export const largeSize = "2.4rem";
-export const greatSize = "4.0rem";
+export const hugeSize = "3.4rem";
+export const greatSize = "4.8rem";
+export const massiveSize = "6.4rem";
