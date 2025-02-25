97a13e58983fd56b018d50aa57233c2ab81e8cb5
James Smith <james.smith@djalbat.com>
2025-02-25 20:12:11 +0000

    Exported the custom event types.

--- src/customEventTypes.js
+++ src/customEventTypes.js
@@ -3,3 +3,9 @@
 export const UPDATE_CUSTOM_EVENT_TYPE = "update";
 export const CHANGE_CUSTOM_EVENT_TYPE = "change";
 export const SELECT_CUSTOM_EVENT_TYPE = "select";
+
+export default {
+  UPDATE_CUSTOM_EVENT_TYPE,
+  CHANGE_CUSTOM_EVENT_TYPE,
+  SELECT_CUSTOM_EVENT_TYPE
+};
